afcb7f64e85ef26d166a2fba4b0ab4743568f74b
T-DEV24 <164579622+T-DEV24@users.noreply.github.com>
2026-01-25 03:04:18 +0100

    Add roles and user journeys to about page

--- Views/front/about.php
+++ Views/front/about.php
@@ -10,5 +10,12 @@
                 <li>Monétisation transparente via la boutique PDF.</li>
             </ul>
         </div>
+        <div class="info-card">
+            <h3>Rôles et parcours</h3>
+            <ul>
+                <li>Rôles confirmés : Admin et Rédacteur.</li>
+                <li>Parcours utilisateurs : visiteur, inscrit, acheteur.</li>
+            </ul>
+        </div>
     </div>
 </section>
